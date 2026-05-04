94fbb9858c0a6bbd53b1fac358263c7c60bf7e8f
Andrews Matsubara <andrewsmatsubara@gmail.com>
2026-05-04 01:24:49 -0300

    add: multiple components

--- main.ts
+++ main.ts
@@ -1,11 +1,20 @@
 import "@arcgis/core/assets/esri/themes/light/main.css";
 import "@arcgis/map-components/dist/arcgis-map-components/arcgis-map-components.css";
 import "@arcgis/coding-components/main.css";
+import "@arcgis/charts-components/dist/arcgis-charts-components/arcgis-charts-components.css";
+import "@arcgis/common-components/main.css";
+import "@esri/calcite-components/main.css";
 import { defineCustomElements } from "@arcgis/map-components/dist/loader";
 import { defineCustomElements as defineCodingElements } from "@arcgis/coding-components/dist/loader";
+import { defineCustomElements as defineChartsElements } from "@arcgis/charts-components/dist/loader";
+import { defineCustomElements as defineCommonElements } from "@arcgis/common-components/loader";
+import { defineCustomElements as defineCalciteElements } from "@esri/calcite-components/loader";
 
 defineCustomElements(window);
 defineCodingElements(window);
+defineChartsElements(window);
+defineCommonElements(window);
+defineCalciteElements(window);
 
 const init = async () => {
   const viewElement = document.querySelector("arcgis-map");
